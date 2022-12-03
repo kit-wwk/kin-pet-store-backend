dd3ea6797d3f326f6c5eb449ef6ad40a857ea172
Wong Wai Kit <21130151+kitwong607@users.noreply.github.com>
2022-12-03 21:54:58 +0800

    feat: impl basic spring secuity
    TODO: OAuth2 impl later

---
+++ src/main/java/com/kinpetstore/restbackend/config/SecurityConfig.java
@@ -0,0 +1,66 @@
+package com.kinpetstore.restbackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.cors().and()
+                .csrf().disable()
+
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+
+                .headers()
+                .contentTypeOptions().and()
+                .xssProtection().and()
+                .cacheControl().and()
+                .httpStrictTransportSecurity().and()
+                .frameOptions().and()
+                .and()
+
+                .authorizeHttpRequests()
+                .requestMatchers("/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .logout()
+                .logoutUrl("/logout");
+        //TODO: impl oAuth2 later
+//                .and()
+//                .oauth2ResourceServer()
+//                .jwt();
+        return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of(
+                        "http://localhost:3000",
+                        "http://127.0.0.1:3000"
+                )
+        );
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
